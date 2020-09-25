76d7536be975d2890ad49920f342124093428669
chnry <ch@chnry.net>
2020-09-25 14:09:39 +0200

    initialisation off all variables that are shared between multiple modules

--- template_nozori/src/nozori_84.cpp
+++ template_nozori/src/nozori_84.cpp
@@ -72,6 +72,7 @@ struct Nozori_all : Module {
 
     void onAdd() override {
         SR_needed = 96000.;
+        init_variable();
         init_random();
         module_function_init_();
         lights[TEXT_LIGHT_48].setBrightness(1.f); 
